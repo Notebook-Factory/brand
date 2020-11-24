763a774f66e3bab2806ef0e557531dc70bd71e6e
Agah <agahkarakuzu@gmail.com>
2020-11-24 13:03:13 -0500

    Update insertLogo.py

--- insertLogo.py
+++ insertLogo.py
@@ -1,6 +1,6 @@
 def insertLogo(fig,scX,scY,posX,posY):
     fig.layout.images = [dict(source="https://raw.githubusercontent.com/notebook-factory/brand/main/neurolibre-icon-red.png",xref="paper", yref="paper",x=posX, y= posY,
             sizex=scX, sizey=scY, opacity = 0.8)]
-    fig.add_annotation(x=posX,y=posY,xref="paper",yref="paper",text="<a href=\"https://neurolibre.com\" target=\"_blank\" style=\"color:gray!important\">NeuroLibre</a>",
+    fig.add_annotation(x=posX-0.02,y=posY,xref="paper",yref="paper",text="<a href=\"https://neurolibre.com\" target=\"_blank\" style=\"color:gray!important\">NeuroLibre</a>",
            showarrow = False, font=dict(size=8, color="gray"))
     return fig
